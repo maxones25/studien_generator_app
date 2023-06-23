f77481af55150edd52ead77818cc08dcd0fd2184
k.mannweiler <2012491@stud.hs-mannheim.de>
2023-06-23 16:47:59 +0200

    removed not existing modules

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,5 +1,3 @@
 export * from './auth/auth.module';
 export * from './directors/directors.module';
-export * from './groups/groups.module';
-export * from './participants/participants.module';
 export * from './studies/studies.module';
