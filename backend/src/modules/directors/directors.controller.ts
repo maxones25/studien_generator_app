9c4242c776ef85953ab4bf57c1482b1ac76d69f0
k.mannweiler <2012491@stud.hs-mannheim.de>
2023-06-23 14:58:46 +0200

    added Director controller and service

--- backend/src/modules/directors/directors.controller.ts
+++ backend/src/modules/directors/directors.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Put, Request } from '@nestjs/common';
+import { DirectorsService } from './directors.service';
+import { Types } from '../../decorators/type.decorator';
+import { Director } from '../../entities/director.entity';
 
 @Controller('directors')
-export class DirectorsController {}
+export class DirectorsController {
+    constructor(private readonly directorsService: DirectorsService) {}
+
+    @Types('director')
+    @Get()
+    async findAll() {
+        return this.directorsService.findAll();
+    }
+
+    @Types('director')
+    @Delete()
+    async delete(@Request() req) {
+        return this.directorsService.delete(req.payload.directorId);
+    }
+
+    @Types('director')
+    @Put()
+    async update(
+        @Request() req,
+        @Body()updatedDirector: Partial<Director>,
+        ) {
+        return this.directorsService.update(req.payload.directorId, updatedDirector)
+    }
+}
+
