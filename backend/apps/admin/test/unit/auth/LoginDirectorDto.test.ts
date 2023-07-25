25646f7ba806351035a3a5673e1e7d385c2fdb05
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-07-25 14:30:31 +0200

    Create LoginDirectorDto.test.ts

---
+++ backend/apps/admin/test/unit/auth/LoginDirectorDto.test.ts
@@ -0,0 +1,68 @@
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+import { CreateGroupDto } from '@admin/groups/dtos/CreateGroupDto';
+import { LoginDirectorDto } from '@admin/auth/dtos/LoginDirectorDto';
+import fakeData from '@test/fakeData';
+
+describe('LoginDirectorDto', () => {
+  it('should pass validation with a valid name', async () => {
+    const director = fakeData.director();
+
+    const dto = plainToClass(LoginDirectorDto, {
+      email: director.email,
+      password: director.password,
+    });
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBe(0);
+  });
+
+  it('should fail validation if email is empty', async () => {
+    const dto = plainToClass(LoginDirectorDto, {
+      email: '',
+      password: fakeData.director().password,
+    });
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors[0].constraints).toHaveProperty('isNotEmpty');
+  });
+
+  it('should fail validation if email is not a string', async () => {
+    const dto = plainToClass(CreateGroupDto, {
+      email: 123,
+      password: fakeData.director().password,
+    });
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors[0].constraints).toHaveProperty('isString');
+  });
+
+  it('should fail validation if password is empty', async () => {
+    const dto = plainToClass(LoginDirectorDto, {
+      email: fakeData.director().email,
+      password: '',
+    });
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors[0].constraints).toHaveProperty('isNotEmpty');
+  });
+
+  it('should fail validation if password is not a string', async () => {
+    const dto = plainToClass(CreateGroupDto, {
+      email: fakeData.director().email,
+      password: 123,
+    });
+
+    const errors = await validate(dto);
+
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors[0].constraints).toHaveProperty('isString');
+  });
+});
