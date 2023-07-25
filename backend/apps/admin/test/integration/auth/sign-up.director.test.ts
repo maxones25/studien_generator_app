9611705d58d4337955d126ea3069fa1a05322992
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-07-25 16:39:48 +0200

    Create sign-up.director.test.ts

---
+++ backend/apps/admin/test/integration/auth/sign-up.director.test.ts
@@ -0,0 +1,117 @@
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { createApp, getEnv } from '@test/utils';
+import fakeData, { director } from '@test/fakeData';
+import { AppModule } from '@admin/app.module';
+import { validateUUID } from '@shared/modules/uuid/uuid';
+
+describe('signUp director', () => {
+  let app: INestApplication;
+  let activationPassword: string;
+
+  beforeAll(async () => {
+    app = await createApp(AppModule);
+    activationPassword = await getEnv(app, 'ACTIVATION_PASSWORD');
+  });
+
+  it('should create a new director successfully', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword,
+      })
+      .expect(201)
+      .then((res) => {
+        expect(validateUUID(res.text)).toBeTruthy();
+      });
+  });
+
+  it('should fail because email already exists', async () => {
+    const director = fakeData.director();
+
+    await request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...director,
+        activationPassword,
+      })
+      .expect(201);
+
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...director,
+        activationPassword,
+      })
+      .expect(409);
+  });
+
+  it('should fail because email is not valid', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword,
+        email: 'invalidemail',
+      })
+      .expect(400);
+  });
+
+  it('should fail because firstName is missing', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword,
+        firstName: undefined,
+      })
+      .expect(400);
+  });
+
+  it('should fail because lastName is missing', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword,
+        lastName: undefined,
+      })
+      .expect(400);
+  });
+
+  it('should fail because password is missing', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword,
+        password: undefined,
+      })
+      .expect(400);
+  });
+
+  it('should fail because activationPassword is missing', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword: undefined,
+      })
+      .expect(400);
+  });
+
+  it('should fail because activationPassword is incorrect', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signUp')
+      .send({
+        ...fakeData.director(),
+        activationPassword: "wrong-password",
+      })
+      .expect(401);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
