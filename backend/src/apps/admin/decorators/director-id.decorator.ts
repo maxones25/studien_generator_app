5a8a4101d9248122650d656ec2542bcc8ba2657c
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-07-25 09:11:28 +0200

    director id unit tests

--- backend/src/apps/admin/decorators/director-id.decorator.ts
+++ backend/src/apps/admin/decorators/director-id.decorator.ts
@@ -8,8 +8,6 @@ export const DirectorId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const request = ctx.switchToHttp().getRequest();
 
-    console.log('p', request?.payload);
-
     if (!request?.payload?.directorId) throw new UnauthorizedException();
 
     return request.payload.directorId as string;
