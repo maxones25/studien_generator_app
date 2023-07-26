bd1b06210d397c1880e298cd3bc04a02fc46a599
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-07-26 10:53:04 +0200

    Update entities.module.ts

--- backend/apps/admin/src/entities/entities.module.ts
+++ backend/apps/admin/src/entities/entities.module.ts
@@ -3,10 +3,13 @@ import { EntitiesController } from './entities.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Entity } from '@entities/entity.entity';
 import entitiesProviders from './entities.providers';
+import { EntityGuard } from './entity.guard';
+import { EntitiesRepository } from './entities.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Entity])],
   providers: entitiesProviders,
   controllers: [EntitiesController],
+  exports: [EntityGuard, EntitiesRepository]
 })
 export class EntitiesModule {}
