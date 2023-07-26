b6eac9aba5ea9db0de7e9abcb378018cffeeb65f
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-07-26 09:54:15 +0200

    move

--- backend/apps/admin/src/studies/studies.module.ts
+++ backend/apps/admin/src/studies/studies.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudiesController } from './studies.controller';
 import { Study } from '@entities/study.entity';
 import studiesProviders from './studies.providers';
+import { StudyMembersModule } from './members/study-members.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Study])],
+  imports: [TypeOrmModule.forFeature([Study]), StudyMembersModule],
   providers: studiesProviders,
   controllers: [StudiesController],
 })
