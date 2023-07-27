e57aa53bc57248f5b2c6f120336ffb60cb352773
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-07-27 17:52:47 +0200

    add form guard

--- backend/apps/admin/src/forms/entities/form-entities.controller.ts
+++ backend/apps/admin/src/forms/entities/form-entities.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Post, Param, Get, Body, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Param,
+  Get,
+  Body,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateFormEntityDto } from './dtos/CreateFormEntityDto';
 import { FormEntitiesService } from './form-entities.service';
 import { Roles } from '@admin/roles/roles.decorator';
 import { ValidateIdPipe } from '@shared/pipes/validate-id.pipe';
 import { FormEntityGuard } from './form-entity.guard';
+import { FormGuard } from '../form.guard';
 
 @Controller('studies/:studyId/forms/:formId/entities')
-@UseGuards(FormEntityGuard)
+@UseGuards(FormGuard, FormEntityGuard)
 export class FormEntitiesController {
   constructor(private readonly formEntitiesService: FormEntitiesService) {}
 
--- backend/apps/admin/src/forms/entities/form-entities.module.ts
+++ backend/apps/admin/src/forms/entities/form-entities.module.ts
@@ -3,9 +3,10 @@ import { FormEntitiesController } from './form-entities.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormEntity } from '@entities/form-entity.entity';
 import formEntitiesProviders from './form-entities.providers';
+import { FormsModule } from '../forms.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FormEntity])],
+  imports: [TypeOrmModule.forFeature([FormEntity]), FormsModule],
   controllers: [FormEntitiesController],
   providers: formEntitiesProviders,
 })
