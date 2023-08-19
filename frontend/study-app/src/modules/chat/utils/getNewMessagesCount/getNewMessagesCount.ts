4825fbfa82a7e4370bf402374f5b4a3d2062a972
k.mannweiler <2012491@stud.hs-mannheim.de>
2023-08-19 22:37:04 +0200

    show unread messages

--- frontend/study-app/src/modules/chat/utils/getNewMessagesCount/getNewMessagesCount.ts
+++ frontend/study-app/src/modules/chat/utils/getNewMessagesCount/getNewMessagesCount.ts
@@ -4,9 +4,9 @@ export const getNewMessagesCount = () => {
   const messages = useGetChatMessages().data;
 
   if (messages) {
-    const incomingMessages = messages.filter((message) => message.participantId === undefined);
+    const incomingMessages = messages.filter((message) => message.directorName);
     if (incomingMessages === undefined) return 0; 
-    return incomingMessages.filter((message) => message.readAt === undefined).length;
+    return incomingMessages.filter((message) => !message.readAt).length;
   }
   return 0
 }
