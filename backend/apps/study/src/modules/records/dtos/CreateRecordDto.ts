9bf31ac3e0e9182e6529f3c79258e8da799d51b4
k.mannweiler <2012491@stud.hs-mannheim.de>
2023-08-24 13:13:50 +0200

    CaseSensitiv

---
+++ backend/apps/study/src/modules/records/dtos/CreateRecordDto.ts
@@ -0,0 +1,46 @@
+import { Type } from "class-transformer";
+import { 
+  ArrayMinSize, 
+  IsArray, 
+  IsDate, 
+  IsNotEmpty, 
+  IsOptional, 
+  IsString, 
+  IsUUID, 
+  ValidateNested 
+} from "class-validator";
+
+export class CreateRecordDto {
+  @IsUUID()
+  id: string
+
+  @IsOptional()
+  @IsUUID()
+  taskId?: string;
+
+  @IsUUID()
+  formId: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  failureReason?: string;
+
+  @Type(() => Date)
+  @IsDate()
+  createdAt: Date;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => RecordFieldDto)
+  @ValidateNested({ each: true })
+  fields: RecordFieldDto[];
+}
+
+export class RecordFieldDto {
+  @IsUUID()
+  entityFieldId: string;
+
+  @IsNotEmpty()
+  value: any;
+}
