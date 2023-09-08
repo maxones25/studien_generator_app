16d36cc25ae7a79540906c63864b7258303f58e0
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-08 23:55:25 +0200

    Update login-director.test.ts

--- backend/apps/admin/test/integration/auth/login-director.test.ts
+++ backend/apps/admin/test/integration/auth/login-director.test.ts
@@ -6,7 +6,6 @@ import { JwtService } from '@nestjs/jwt';
 import request from 'supertest';
 import { SignupDirectorDto } from '@admin/directors/dtos/SignupDirectorDto';
 
-// rtest
 describe('login director', () => {
   let app: INestApplication;
   let directorId: string;
