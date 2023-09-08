b19eb415fc8921dd882f6b9ee2fed720cfa4d91d
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-08 18:13:04 +0200

    Update index.ts

--- backend/libs/entities/src/schema/index.ts
+++ backend/libs/entities/src/schema/index.ts
@@ -1,4 +1,3 @@
-export * from './study/study.entity';
 export * from './director/director.entity';
 export * from './group/group.entity';
 export * from './study/study-member.entity';
@@ -24,3 +23,4 @@ export * from './study/study-attribute.entity';
 export * from './participant/participant-notification.entity';
 export * from './group/form-schedule-attribute.entity';
 export * from './appointment/appointment.entity';
+export * from './study/study.entity';
