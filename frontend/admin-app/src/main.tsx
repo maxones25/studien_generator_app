5d9515c092268475877a5f4c26fa27bf90340b4f
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-08 15:39:19 +0200

    Update main.tsx

--- frontend/admin-app/src/main.tsx
+++ frontend/admin-app/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// test
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
