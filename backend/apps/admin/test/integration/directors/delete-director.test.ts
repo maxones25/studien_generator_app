1ff9f6aa4c1a65a4cacf5f96bc0482a635b108f5
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-12 20:41:09 +0200

    add delete director api tests

---
+++ backend/apps/admin/test/integration/directors/delete-director.test.ts
@@ -0,0 +1,138 @@
+import { AppModule } from '@admin/app.module';
+import { INestApplication } from '@nestjs/common';
+import { createApp } from '@test/app/createApp';
+import { getAdminAccessToken } from '@test/auth/loginAdmin';
+import {
+  getDirectorAccessToken,
+  loginDirector,
+} from '@test/auth/loginDirector';
+import { deleteDirector } from '@test/director/deleteDirector';
+import { getDirectors } from '@test/director/getDirectors';
+import { getMe } from '@test/director/getMe';
+import { createDirector } from '@test/director/signUpDirector';
+import fakeData from '@test/fakeData';
+import { TEST_DIRECTOR } from '@test/testData';
+
+describe('delete director', () => {
+  let app: INestApplication;
+  let accessToken: string;
+
+  beforeAll(async () => {
+    app = await createApp(AppModule);
+    accessToken = await getAdminAccessToken(app);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should hard delete director', async () => {
+    const data = fakeData.director();
+
+    const director = await createDirector(app, data);
+
+    const directorAT = await getDirectorAccessToken(
+      app,
+      data.email,
+      data.password,
+    );
+
+    await deleteDirector(app, {
+      accessToken,
+      directorId: director.id,
+      data: { hardDelete: true },
+    }).expect(200);
+
+    await getMe(app, directorAT).expect(401);
+
+    return loginDirector(app, data.email, data.password).expect(401);
+  });
+
+  it('should soft delete director', async () => {
+    const data = fakeData.director();
+
+    const director = await createDirector(app, data);
+
+    await loginDirector(app, data.email, data.password).expect(200);
+
+    await deleteDirector(app, {
+      accessToken,
+      directorId: director.id,
+      data: { hardDelete: false },
+    }).expect(200);
+
+    await getDirectors(app, accessToken)
+      .expect(200)
+      .then((res) => {
+        const directors = res.body;
+        expect(Array.isArray(directors)).toBeTruthy();
+        expect(directors.length).toBeGreaterThan(0);
+
+        const found = directors.find((d) => d.id === director.id);
+
+        expect(found).not.toBeUndefined();
+        expect(found.deletedAt).not.toBeNull();
+      });
+
+    return loginDirector(app, data.email, data.password).expect(401);
+  });
+
+  it('should fail because not authenticated', async () => {
+    const director = await createDirector(app);
+
+    await deleteDirector(app, {
+      accessToken: undefined,
+      directorId: director.id,
+      data: { hardDelete: true },
+    }).expect(401);
+  });
+
+  it('should fail because director id invalid', async () => {
+    await deleteDirector(app, {
+      accessToken,
+      directorId: 'invalid-id',
+      data: { hardDelete: true },
+    }).expect(401);
+  });
+
+  it('should fail because director id is unknown', async () => {
+    const directorId = fakeData.id();
+    await deleteDirector(app, {
+      accessToken,
+      directorId,
+      data: { hardDelete: true },
+    }).expect(401);
+  });
+
+  it('should fail because hardDelete is missing', async () => {
+    const director = await createDirector(app);
+    await deleteDirector(app, {
+      accessToken,
+      directorId: director.id,
+      data: {},
+    }).expect(400);
+  });
+
+  it('should fail because hardDelete wrong type', async () => {
+    const director = await createDirector(app);
+    await deleteDirector(app, {
+      accessToken,
+      directorId: director.id,
+      data: { hardDelete: 'test' },
+    }).expect(400);
+  });
+
+  it('should fail because accessToken is director token', async () => {
+    const director = await createDirector(app);
+    const accessToken = await getDirectorAccessToken(
+      app,
+      TEST_DIRECTOR.MAX.EMAIL,
+      TEST_DIRECTOR.MAX.PASSWORD,
+    );
+    await deleteDirector(app, {
+      accessToken,
+      directorId: director.id,
+      data: { hardDelete: true },
+    }).expect(401);
+  });
+});
