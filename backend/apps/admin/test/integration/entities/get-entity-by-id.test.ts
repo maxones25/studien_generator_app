b7a9cb6a12d43b38e3eff4e57b2e074c276cfc94
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-13 00:53:08 +0200

    Update get-entity-by-id.test.ts

--- backend/apps/admin/test/integration/entities/get-entity-by-id.test.ts
+++ backend/apps/admin/test/integration/entities/get-entity-by-id.test.ts
@@ -2,13 +2,12 @@ import { TEST_DIRECTOR } from '@test/testData';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '@admin/app.module';
 import fakeData from '@test/fakeData';
-import { createEntity, createEntityId } from '@test/entities/createEntity';
-import { getEntities } from '@test/entities/getEntities';
+import { createEntityId } from '@test/entities/createEntity';
 import { createStudyId } from '@test/studies/createStudy';
 import { getDirectorAccessToken } from '@test/auth/loginDirector';
 import { createApp } from '@test/app/createApp';
 import { getEntityById } from '@test/entities/getEntityById';
-import { createField, createFieldId } from '@test/entities/fields/createField';
+import { createFieldId } from '@test/entities/fields/createField';
 import { getAdminAccessToken } from '@test/auth/loginAdmin';
 
 describe('Get Entities', () => {
