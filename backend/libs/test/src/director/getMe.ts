3fa462be81b2f8c88822d6b8f29dda5986dd7bc1
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-12 23:11:16 +0200

    add getDirectorById

--- backend/libs/test/src/director/getMe.ts
+++ backend/libs/test/src/director/getMe.ts
@@ -1,7 +1,17 @@
 import { INestApplication } from '@nestjs/common';
+import { validateUUID } from '@shared/modules/uuid/uuid';
 import request from 'supertest';
 
-export const getMe = (app: INestApplication, accessToken: string) =>
+export const getMe = (app: INestApplication, accessToken: any) =>
   request(app.getHttpServer())
     .get(`/directors/me`)
     .set('Authorization', `Bearer ${accessToken}`);
+
+export const getDirectorById = (app: INestApplication, accessToken: string) => new Promise<any>((resolve, reject) => {
+  getMe(app, accessToken)
+  .expect(200).then(res => {
+    const director = res.body;
+    expect(validateUUID(director.id)).toBeTruthy();
+    resolve(director);
+  }).catch(reject);
+})
