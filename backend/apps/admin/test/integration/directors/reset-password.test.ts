3e03c28b53942fcdb9d06b12ccae35d3baa82210
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-12 23:40:59 +0200

    add reset director password api tests

--- backend/apps/admin/test/integration/directors/reset-password.test.ts
+++ backend/apps/admin/test/integration/directors/reset-password.test.ts
@@ -11,6 +11,8 @@ import fakeData from '@test/fakeData';
 import { updateDirector } from '@test/director/updateDirector';
 import { Director } from '@entities/core/director/Director';
 import { resetPassword } from '@test/director/resetPassword';
+import { TEST_DIRECTOR } from '@test/testData';
+import { getDirectorById } from '@test/director/getMe';
 
 describe('reset director password', () => {
   let app: INestApplication;
@@ -45,10 +47,97 @@ describe('reset director password', () => {
     const director = await createDirector(app);
     const password = fakeData.password();
 
-    await updateDirector(app, {
+    await resetPassword(app, {
       accessToken: undefined,
       directorId: director.id,
       data: { password },
     }).expect(401);
+  });
+
+  it('should fail because directorId is invalid', async () => {
+    await createDirector(app);
+    const password = fakeData.password();
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: 'invalid-id',
+      data: { password },
+    }).expect(401);
+  });
+
+  it('should fail because directorId is unknown', async () => {
+    await createDirector(app);
+    const password = fakeData.password();
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: fakeData.id(),
+      data: { password },
+    }).expect(401);
+  });
+
+  it('should fail because password is missing', async () => {
+    const director = await createDirector(app);
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: director.id,
+      data: {},
+    }).expect(400);
+  });
+
+  it('should fail because password is empty', async () => {
+    const director = await createDirector(app);
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: director.id,
+      data: { password: '' },
+    }).expect(400);
+  });
+
+  it('should fail because password is too short', async () => {
+    const director = await createDirector(app);
+    const password = fakeData.password(7);
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: director.id,
+      data: { password },
+    }).expect(400);
+  });
+
+  it('should fail because director is authenticated', async () => {
+    const accessToken = await getDirectorAccessToken(
+      app,
+      TEST_DIRECTOR.MAX.EMAIL,
+      TEST_DIRECTOR.MAX.PASSWORD,
+    );
+
+    const director = await createDirector(app);
+    const password = fakeData.password();
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: director.id,
+      data: { password },
+    }).expect(401);
+  });
+
+  it('should not update id', async () => {
+    const director = await createDirector(app);
+    const password = fakeData.password();
+
+    await resetPassword(app, {
+      accessToken,
+      directorId: director.id,
+      data: { password, id: fakeData.id() },
+    }).expect(200);
+
+    const directorAT = await getDirectorAccessToken(app, director.email, password);
+
+    const updatedDirector = await getDirectorById(app, directorAT);
+
+    expect(updatedDirector.id).toBe(director.id);
   });
 });
