3223710b21ff91ba4a8a4d96824cb2cd1da0967b
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-13 11:48:35 +0200

    refactor: use new request api

--- backend/libs/test/src/forms/changeFormName.ts
+++ backend/libs/test/src/forms/changeFormName.ts
@@ -1,7 +1,6 @@
-import { INestApplication } from '@nestjs/common';
 import { IApp } from '@test/app/createApp';
+import { request } from '@test/app/request';
 import { StudyRequestOptions } from '@test/types';
-import request from 'supertest';
 
 export interface ChangeFormNameOptions extends StudyRequestOptions {
   formId: any;
@@ -11,30 +10,10 @@ export interface ChangeFormNameOptions extends StudyRequestOptions {
 export const changeFormName = (
   app: IApp,
   { accessToken, studyId, formId, data }: ChangeFormNameOptions,
-) => {
-  const r = request(app.getHttpServer()).post('/forms/changeName');
-
-  if (accessToken !== undefined) {
-    r.set('Authorization', `Bearer ${accessToken}`);
-  }
-
-  const qs: any = {}
-
-  if (studyId !== undefined) {
-    qs.studyId = studyId
-  }
-
-  if (formId !== undefined) {
-    qs.formId = formId
-  }
-
-  if(Object.keys(qs).length > 0) {
-    r.query(qs)
-  }
-
-  if (data !== undefined) {
-    r.send(data);
-  }
-
-  return r;
-};
+) =>
+  request(app).command({
+    path: '/forms/changeName',
+    accessToken,
+    query: { studyId, formId },
+    data,
+  });
