68b99ae5e8ac605001bc3b0d4aa7aaa39b7e97fc
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-13 22:26:03 +0200

    feat: add default data

--- backend/libs/test/src/entities/createEntity.ts
+++ backend/libs/test/src/entities/createEntity.ts
@@ -1,14 +1,15 @@
 import { IApp } from '@test/app/createApp';
 import { StudyRequestOptions } from '../types';
 import { request } from '@test/app/request';
+import fakeData from '@test/fakeData';
 
 export interface CreateEntityOptions extends StudyRequestOptions {
-  data: object;
+  data?: object;
 }
 
 export const createEntity = (
   app: IApp,
-  { accessToken, studyId, data }: CreateEntityOptions,
+  { accessToken, studyId, data = fakeData.entity() }: CreateEntityOptions,
 ) =>
   request(app).command({
     path: `/entities/createEntity`,
@@ -19,7 +20,7 @@ export const createEntity = (
 
 export const createEntityId = (
   app: IApp,
-  { accessToken, studyId, data }: CreateEntityOptions,
+  { accessToken, studyId, data = fakeData.entity() }: CreateEntityOptions,
 ) =>
   new Promise<string>((resolve, reject) => {
     createEntity(app, { accessToken, studyId, data })
