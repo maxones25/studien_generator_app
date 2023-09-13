aa0a6439091830b3d19525ad1a410f59ea8425fa
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-13 11:10:30 +0200

    fix: call initializeTransactionalContext before start

--- backend/libs/test/src/app/createApp.ts
+++ backend/libs/test/src/app/createApp.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ValidationPipe } from '@nestjs/common';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 export const createApp = async (AppModule: any) => {
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
+
+  initializeTransactionalContext()
 
   const app = moduleFixture.createNestApplication();
 
