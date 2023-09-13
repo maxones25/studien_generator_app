5331e78bfa2abda90d99655ba7cf898692e84cee
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-13 12:32:22 +0200

    add change form name api tests

---
+++ backend/apps/admin/test/integration/forms/change-form-name.test.ts
@@ -0,0 +1,199 @@
+import { TEST_DIRECTOR } from '@test/testData';
+import { AppModule } from '@admin/app.module';
+import { createStudyId } from '@test/studies/createStudy';
+import { createFormId } from '@test/forms/createForm';
+import fakeData from '@test/fakeData';
+import { getDirectorAccessToken } from '@test/auth/loginDirector';
+import { IApp, createApp } from '@test/app/createApp';
+import { changeFormName } from '@test/forms/changeFormName';
+import { getFormById } from '@test/forms/getFormById';
+import { getAdminAccessToken } from '@test/auth/loginAdmin';
+
+describe('change form name', () => {
+  let app: IApp;
+  let accessToken: string;
+  let otherAccessToken: string;
+  let studyId: string;
+
+  beforeAll(async () => {
+    app = await createApp(AppModule);
+
+    accessToken = await getDirectorAccessToken(
+      app,
+      TEST_DIRECTOR.MAX.EMAIL,
+      TEST_DIRECTOR.MAX.PASSWORD,
+    );
+
+    otherAccessToken = await getDirectorAccessToken(
+      app,
+      TEST_DIRECTOR.JOHN.EMAIL,
+      TEST_DIRECTOR.JOHN.PASSWORD,
+    );
+
+    studyId = await createStudyId(app, { accessToken });
+  });
+
+  afterAll(async () => {
+    return await app.close();
+  });
+
+  it('should change name', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+
+    const name = fakeData.form().name;
+
+    await changeFormName(app, {
+      accessToken,
+      studyId,
+      formId,
+      data: { name },
+    }).expect(200);
+
+    return getFormById(app, { accessToken, studyId, formId })
+      .expect(200)
+      .then((res) => {
+        const form = res.body;
+
+        expect(form.name).toBe(name);
+      });
+  });
+
+  it('should fail because unauthorized', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken: undefined,
+      studyId,
+      formId,
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because admin is authorized', async () => {
+    const adminAccessToken = await getAdminAccessToken(app);
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken: adminAccessToken,
+      studyId,
+      formId,
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because director is not member of study', async () => {
+    const formId = await createFormId(app, {
+      accessToken,
+      studyId,
+    });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken: otherAccessToken,
+      studyId,
+      formId,
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because studyId missing', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId: undefined,
+      formId,
+      data: { name },
+    }).expect(400);
+  });
+
+  it('should fail because studyId invalid', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId: 'undefined123',
+      formId,
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because studyId unknown', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId: fakeData.id(),
+      formId,
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because formId missing', async () => {
+    await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId,
+      formId: undefined,
+      data: { name },
+    }).expect(400);
+  });
+
+  it('should fail because formId invalid', async () => {
+    await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId,
+      formId: 'undsdfsdefined',
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because formId unknown', async () => {
+    await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId,
+      formId: fakeData.id(),
+      data: { name },
+    }).expect(401);
+  });
+
+  it('should fail because name missing', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId,
+      formId,
+      data: {},
+    }).expect(400);
+  });
+
+  it('should fail because name is empty', async () => {
+    const formId = await createFormId(app, { accessToken, studyId });
+    const name = fakeData.form().name;
+    return changeFormName(app, {
+      accessToken,
+      studyId,
+      formId,
+      data: { name: '' },
+    }).expect(400);
+  });
+
+  it('should fail because name already exists', async () => {
+    const data = fakeData.form();
+    await createFormId(app, { accessToken, studyId, data });
+
+    const formId = await createFormId(app, { accessToken, studyId });
+
+    return changeFormName(app, {
+      accessToken,
+      studyId,
+      formId,
+      data: { name: data.name },
+    }).expect(422);
+  });
+});
