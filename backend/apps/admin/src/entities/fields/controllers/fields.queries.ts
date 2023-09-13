813875c708e3b3c370a1f316567cf12c20110d5f
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-13 22:25:46 +0200

    fix: add StudyGuard

--- backend/apps/admin/src/entities/fields/controllers/fields.queries.ts
+++ backend/apps/admin/src/entities/fields/controllers/fields.queries.ts
@@ -3,8 +3,10 @@ import { Roles } from '@admin/roles/roles.decorator';
 import { EntityQueryDto } from '@admin/entities/entities/dtos/EntityQueryDto';
 import { FieldsService } from '../fields.service';
 import { EntityGuard } from '@admin/entities/entities/entity.guard';
+import { StudyGuard } from '@admin/studies/studies/guards/study.guard';
 
 @Controller('entities')
+@UseGuards(StudyGuard)
 export class FieldsQueries {
   constructor(
     @Inject(FieldsService)
