c2bbb51880269be1d439c22aa051bed0cd9a7dd1
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-16 15:57:57 +0200

    refactor: rename controller to api

--- backend/apps/admin/src/directors/directors.app.ts
+++ backend/apps/admin/src/directors/directors.app.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DirectorsCommands } from './infrastructure/http/controllers/directors.commands';
+import { DirectorsCommands } from './infrastructure/http/api/directors.commands';
 import { DirectorsModule } from './directors.module';
-import { AuthCommands } from './infrastructure/http/controllers/auth.commands';
-import { DirectorsQueries } from './infrastructure/http/controllers/directors.queries';
+import { AuthCommands } from './infrastructure/http/api/auth.commands';
+import { DirectorsQueries } from './infrastructure/http/api/directors.queries';
 
 @Module({
   imports: [DirectorsModule],
