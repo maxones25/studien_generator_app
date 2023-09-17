31e31a6b822bd94cc6b0bc82023de59f04f406a0
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-17 19:06:35 +0200

    fix: deleted required boolean

--- backend/apps/admin/src/groups/controllers/groups.queries.ts
+++ backend/apps/admin/src/groups/controllers/groups.queries.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, UseGuards, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Query,
+  ParseBoolPipe,
+} from '@nestjs/common';
 import { GroupsService } from '../groups.service';
 import { Roles } from '@admin/members/infrastructure/http';
 import { StudyGuard } from '@admin/studies/studies/guards/study.guard';
 import { StudyQueryDto } from '@admin/studies/studies/dtos/StudyQueryDto';
 import { GroupGuard } from '../guards/group.guard';
 import { GroupQueryDto } from '../dtos/GroupQueryDto';
-import { DeletedDto } from '@shared/modules/records/DeletedDto';
 
 @Controller('groups')
 @UseGuards(StudyGuard)
@@ -16,7 +21,7 @@ export class GroupsQueries {
   @Roles('admin', 'employee')
   async getAll(
     @Query() { studyId }: StudyQueryDto,
-    @Query() { deleted }: DeletedDto,
+    @Query('deleted', ParseBoolPipe) deleted: boolean,
   ) {
     return this.groupsService.getByStudy(studyId, deleted);
   }
