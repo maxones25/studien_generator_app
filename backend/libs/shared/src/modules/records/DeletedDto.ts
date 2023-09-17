b5c83c1940c011d1c456b38ab4478492f2c4052d
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-17 18:11:37 +0200

    fix: only accept true and false strings

--- backend/libs/shared/src/modules/records/DeletedDto.ts
+++ backend/libs/shared/src/modules/records/DeletedDto.ts
@@ -1,6 +1,13 @@
 import { Transform } from 'class-transformer';
+import { IsBoolean } from 'class-validator';
 
 export class DeletedDto {
-  @Transform(({ value }) => value.toLowerCase() === 'true')
+  @IsBoolean()
+  @Transform(({ value }) => {
+    if(value === undefined) return false;
+    if(value === "true") return true
+    if(value === "false") return false;
+    return value;
+  })
   readonly deleted: boolean;
 }
