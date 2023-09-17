40d153cb59717b037738cbdf4cf6856f1ac478f3
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-17 19:32:22 +0200

    fix: change default

--- backend/libs/test/src/groups/getGroups.ts
+++ backend/libs/test/src/groups/getGroups.ts
@@ -2,14 +2,16 @@ import { IApp } from '@test/app/createApp';
 import { request } from '@test/app/request';
 import { StudyRequestOptions } from '@test/types';
 
-export interface GetGroupsOptions extends StudyRequestOptions {}
+export interface GetGroupsOptions extends StudyRequestOptions {
+  deleted?: any;
+}
 
 export const getGroups = (
   app: IApp,
-  { accessToken, studyId }: GetGroupsOptions,
+  { accessToken, studyId, deleted = false }: GetGroupsOptions,
 ) =>
   request(app).query({
     path: '/groups/getByStudy',
     accessToken,
-    query: { studyId },
+    query: { studyId, deleted },
   });
