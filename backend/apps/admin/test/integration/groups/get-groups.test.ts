0326efbda0dc725109548c18937536b49dc934b2
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-18 16:04:52 +0200

    test: add get groups api tests

--- backend/apps/admin/test/integration/groups/get-groups.test.ts
+++ backend/apps/admin/test/integration/groups/get-groups.test.ts
@@ -6,13 +6,18 @@ import { createGroupId } from '@test/groups/createGroup';
 import { getGroups } from '@test/groups/getGroups';
 import { getDirectorAccessToken } from '@test/auth/loginDirector';
 import { IApp, createApp } from '@test/app/createApp';
+import { getAdminAccessToken } from '@test/auth/loginAdmin';
+import fakeData from '@test/fakeData';
+import { deleteGroup } from '@test/groups/deleteGroup';
 
 describe('get groups', () => {
   let app: IApp;
   let accessToken: string;
+  let otherAccessToken: string;
   let studyId: string;
-  let johnStudyId: string;
+  let otherStudyId: string;
   let groupIds: string[];
+  let deletedGroupIds: string[];
 
   beforeAll(async () => {
     app = await createApp(AppModule);
@@ -32,21 +37,34 @@ describe('get groups', () => {
     groupIds.push(await createGroupId(app, { accessToken, studyId }));
     groupIds.push(await createGroupId(app, { accessToken, studyId }));
 
-    const johnAccessToken = await getDirectorAccessToken(
+    const deletedGroupId = await createGroupId(app, { accessToken, studyId });
+
+    await deleteGroup(app, {
+      accessToken,
+      studyId,
+      groupId: deletedGroupId,
+      hardDelete: false,
+    });
+
+    deletedGroupIds = [];
+    deletedGroupIds.push(deletedGroupId);
+
+    otherAccessToken = await getDirectorAccessToken(
       app,
       TEST_DIRECTOR.JOHN.EMAIL,
       TEST_DIRECTOR.JOHN.PASSWORD,
     );
 
-    johnStudyId = await createStudyId(app, { accessToken: johnAccessToken });
+    otherStudyId = await createStudyId(app, { accessToken: otherAccessToken });
 
     await createGroupId(app, {
-      accessToken: johnAccessToken,
-      studyId: johnStudyId,
+      accessToken: otherAccessToken,
+      studyId: otherStudyId,
     });
+
     await createGroupId(app, {
-      accessToken: johnAccessToken,
-      studyId: johnStudyId,
+      accessToken: otherAccessToken,
+      studyId: otherStudyId,
     });
   });
 
@@ -54,20 +72,62 @@ describe('get groups', () => {
     await app.close();
   });
 
-  it('should get all groups', async () => {
-    return getGroups(app, { accessToken, studyId })
+  it('should get groups by study with deleted', async () => {
+    return getGroups(app, { accessToken, studyId, deleted: true })
       .expect(200)
       .then((res) => {
         expect(Array.isArray(res.body)).toBeTruthy();
-        expect(res.body.length).toBe(3);
+        expect(res.body.length).toBeGreaterThan(0);
         res.body.forEach((group) => {
-          expect(groupIds.includes(group.id)).toBeTruthy();
+          expect(
+            [...groupIds, ...deletedGroupIds].includes(group.id),
+          ).toBeTruthy();
           expect(typeof group.name).toBe('string');
         });
       });
   });
 
+  it('should get groups by study without deleted', async () => {
+    return getGroups(app, { accessToken, studyId, deleted: false })
+      .expect(200)
+      .then((res) => {
+        expect(Array.isArray(res.body)).toBeTruthy();
+        expect(res.body.length).toBeGreaterThan(0);
+        res.body.forEach((group) => {
+          expect(groupIds.includes(group.id)).toBeTruthy();
+          expect(deletedGroupIds.includes(group.id)).toBeFalsy();
+          expect(typeof group.name).toBe('string');
+        });
+      });
+  });
+
+  it('should fail because unauthorized', async () => {
+    return getGroups(app, { accessToken: undefined, studyId }).expect(401);
+  });
+
+  it('should fail because admin is authorized', async () => {
+    const adminAccessToken = await getAdminAccessToken(app);
+
+    return getGroups(app, { accessToken: adminAccessToken, studyId }).expect(
+      401,
+    );
+  });
+
   it('should fail because director is not member of study', async () => {
-    return getGroups(app, { accessToken, studyId: johnStudyId }).expect(401);
+    return getGroups(app, { accessToken, studyId: otherStudyId }).expect(401);
+  });
+
+  it('should fail because studyId is missing', async () => {
+    return getGroups(app, { accessToken, studyId: undefined }).expect(400);
+  });
+
+  it('should fail because studyId is invalid', async () => {
+    return getGroups(app, { accessToken, studyId: fakeData.text() }).expect(
+      401,
+    );
+  });
+
+  it('should fail because studyId is unknown', async () => {
+    return getGroups(app, { accessToken, studyId: fakeData.id() }).expect(401);
   });
 });
