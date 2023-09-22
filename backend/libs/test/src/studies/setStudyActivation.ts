b09e9360fbb3bee37afd8f41319dca1a5b5ef5c1
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-09-22 17:36:05 +0200

    feat: add activateStudy

--- backend/libs/test/src/studies/setStudyActivation.ts
+++ backend/libs/test/src/studies/setStudyActivation.ts
@@ -1,6 +1,10 @@
 import { IApp } from '@test/app/createApp';
 import { request } from '@test/app/request';
 import { StudyRequestOptions } from '@test/types';
+import { setStudyStart } from './setStudyStart';
+import datetime from '@shared/modules/datetime/datetime';
+import { setStudyEnd } from './setStudyEnd';
+import { setStudyDuration } from './setStudyDuration';
 
 export interface SetStudyActivationOptions extends StudyRequestOptions {
   isActive: any;
@@ -16,3 +20,38 @@ export const setStudyActivation = (
     query: { studyId },
     data: { isActive },
   });
+
+export interface ActivateStudyOptions extends StudyRequestOptions {
+  startDate: Date;
+  endDate: Date;
+  duration: number;
+}
+
+export const activateStudy = async (
+  app: IApp,
+  { accessToken, studyId, startDate, endDate, duration }: ActivateStudyOptions,
+) => {
+  await setStudyStart(app, {
+    accessToken,
+    studyId,
+    startDate: datetime.formatDate(startDate),
+  }).expect(200);
+
+  await setStudyEnd(app, {
+    accessToken,
+    studyId,
+    endDate: datetime.formatDate(endDate),
+  }).expect(200);
+
+  await setStudyDuration(app, {
+    accessToken,
+    studyId,
+    duration,
+  }).expect(200);
+
+  await setStudyActivation(app, {
+    accessToken,
+    studyId,
+    isActive: true,
+  }).expect(200);
+};
