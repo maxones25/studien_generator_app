fffdbe2f25a4962fa172ba49b65253f3002bb9c1
Max Stock <36995969+maxones25@users.noreply.github.com>
2023-10-01 18:27:32 +0200

    feat: add test data

--- backend/libs/test/src/testData.ts
+++ backend/libs/test/src/testData.ts
@@ -1,11 +1,11 @@
 export const TEST_DIRECTOR = {
   MAX: {
-    ID: "d98cd02e-1ebf-49f4-8b9b-df37ad78911e",
+    ID: 'd98cd02e-1ebf-49f4-8b9b-df37ad78911e',
     EMAIL: 'max.mustermann@test.de',
     PASSWORD: '12345678',
   },
   JOHN: {
-    ID: "91bffd5b-9b0f-49a5-851a-684133d5ac30",
+    ID: '91bffd5b-9b0f-49a5-851a-684133d5ac30',
     EMAIL: 'john.doe@test.de',
     PASSWORD: '12345678',
   },
@@ -18,16 +18,55 @@ export const TEST_STUDY = {
 
 export const TEST_GROUP = {
   ID: '497d0534-de0a-4267-ae00-5221a0c6b6ef',
-}
+  FORM_CONFIG: {
+    ID: 'c2c7c9ed-ef54-4ca9-9106-4c7a0c66af83',
+  },
+};
 
 export const TEST_PARTICIPANT = {
-  ID: "54feae71-a7ae-4752-b9b5-a908e2446d31",
-  PASSWORD: "0123456789AB",
-  SUBSCRIPTION: `{"endpoint":"https://fcm.googleapis.com/fcm/send/coB6AVF4RGw:APA91bGfCxfO9u7-taLZ59fOfLobeUt09t9KLvKWiLHJ344MqWKeMeNoLff6y4EZYaLVuXCfxEGUW4I9kvddtD4NUAWoIUDOqXLgNU8fCA06AC0BJfNpLbnIlo6x4S1I7GLoP_lBCNRk","expirationTime":null,"keys":{"p256dh":"BN5fMo9FWcP7g6WqFPPjRkGlYDHc6td8mWs-hYcq_sDqAFfcbjJHzxUn0lxoxnum13YKGc2LqiLwdDeXXZaf4Vc","auth":"NE9aK9PJ46FzTVqV1y8f1Q"}}`
+  ID: '54feae71-a7ae-4752-b9b5-a908e2446d31',
+  PASSWORD: '0123456789AB',
+  SUBSCRIPTION: `{"endpoint":"https://fcm.googleapis.com/fcm/send/coB6AVF4RGw:APA91bGfCxfO9u7-taLZ59fOfLobeUt09t9KLvKWiLHJ344MqWKeMeNoLff6y4EZYaLVuXCfxEGUW4I9kvddtD4NUAWoIUDOqXLgNU8fCA06AC0BJfNpLbnIlo6x4S1I7GLoP_lBCNRk","expirationTime":null,"keys":{"p256dh":"BN5fMo9FWcP7g6WqFPPjRkGlYDHc6td8mWs-hYcq_sDqAFfcbjJHzxUn0lxoxnum13YKGc2LqiLwdDeXXZaf4Vc","auth":"NE9aK9PJ46FzTVqV1y8f1Q"}}`,
 };
 
 export const TEST_CHAT = {
-  ID: "59ef2c9c-6eaf-4768-b628-d8e875687723",
-}
+  ID: '59ef2c9c-6eaf-4768-b628-d8e875687723',
+};
 
+export const TEST_ENTITY = {
+  ID: 'c2c31a68-6bd7-48df-8d4b-47a95de8920f',
+  FIELDS: {
+    TEXT: {
+      ID: '6f776404-0402-42ca-9649-bace38c52684',
+    },
+    DATE: {
+      ID: 'cba34ffd-f3ce-4d64-928f-02a2f2bc3549',
+    },
+  },
+};
 
+export const TEST_FORM = {
+  ID: '9cc9de50-065f-4ca9-88ad-b1453f51ee3e',
+  PAGE: {
+    ID: '5d791f96-0622-4084-9079-2239c55e5b3e',
+  },
+  COMPONENTS: {
+    TEXTFIELD: {
+      ID: 'e52c9298-8b1e-4881-9572-9765bad2e8ee',
+    },
+    DATEPICKER: {
+      ID: '8eb5a6dd-2652-42cc-8af1-45a267f44a62',
+    },
+  },
+  ENTITY: {
+    ID: 'c118259b-5c92-4cb3-ba51-d5b5a3a5b3b1',
+  },
+  FIELDS: {
+    TEXT: {
+      ID: '84b78ebb-0684-45b1-9663-8087738c6209',
+    },
+    DATE: {
+      ID: 'bd99097c-fdd0-4cac-ac45-e9db68eae858',
+    },
+  },
+};
