46f810b406f35c5a697e9c09de85c0ce36c6bac8
k.mannweiler <2012491@stud.hs-mannheim.de>
2023-10-05 01:50:45 +0200

    Fixed Backend

--- backend/apps/study/src/modules/scheduling/scheduling.module.ts
+++ backend/apps/study/src/modules/scheduling/scheduling.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { SchedulingService } from "./scheduling.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ChatMessage, ParticipantNotification, Task } from "@entities";
+import { Appointment, ChatMessage, ParticipantNotification, Task } from "@entities";
 import { DataService } from "./scheduling-data.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ParticipantNotification, ChatMessage, Task])],
+  imports: [TypeOrmModule.forFeature([ParticipantNotification, ChatMessage, Task, Appointment])],
   providers: [SchedulingService, DataService],
 })
 export class SchedulingModule {}
