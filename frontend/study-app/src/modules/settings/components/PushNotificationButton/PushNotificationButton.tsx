ee582d143fb27b66ddf8b21054e43b77ba1c3c75
k.mannweiler <2012491@stud.hs-mannheim.de>
2023-10-10 13:07:47 +0200

    refactoring

--- frontend/study-app/src/modules/settings/components/PushNotificationButton/PushNotificationButton.tsx
+++ frontend/study-app/src/modules/settings/components/PushNotificationButton/PushNotificationButton.tsx
@@ -1,54 +1,56 @@
+import React, { useState } from 'react';
 import { Button } from '@modules/core/components';
 import { useSnackBarContext } from '@modules/core/contexts';
 import { useSubscribePush } from '@modules/settings/hooks';
 import { urlB64ToUint8Array } from '@modules/settings/utils';
 import { useTheme } from '@mui/material';
-import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY
 
-export interface PushNotificationButtonProps {}
+const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY;
 
-export const PushNotificationButton : React.FC<PushNotificationButtonProps>= ({
+interface PushNotificationButtonProps {}
 
-}) => {
+const PushNotificationButton: React.FC<PushNotificationButtonProps> = () => {
   const { t } = useTranslation();
   const { showError } = useSnackBarContext();
-  const subscribe = useSubscribePush();
-  const permission = window?.Notification?.permission;
-  const [loading, setLoading] = useState(false);
   const { palette } = useTheme();
+  const subscribe = useSubscribePush();
+  const [loading, setLoading] = useState(false);
+
+  const permission = window?.Notification?.permission;
+
+  const getButtonColor = () => {
+    switch (permission) {
+      case 'granted':
+        return palette.success.main;
+      case 'denied':
+        return palette.error.main;
+      default:
+        return palette.grey[500];
+    }
+  };
 
   const onAcceptNotifications = async () => {
-    navigator.serviceWorker.getRegistration().then(async(sw) => {
-      setLoading(true);
-      try {
-        const applicationServerKey = urlB64ToUint8Array(
-          VAPID_PUBLIC_KEY
-        );
-        const options = { applicationServerKey, userVisibleOnly: true };
-        const subscription = await sw?.pushManager.subscribe(options);   
-        subscribe.mutateAsync({
-          subscription: JSON.stringify(subscription)
-        });       
-      } catch (err: any) {
-        showError(err);
-      } finally {
-        setLoading(false);
-      }
-    });
+    setLoading(true);
+    try {
+      const sw = await navigator.serviceWorker.getRegistration();
+      const applicationServerKey = urlB64ToUint8Array(VAPID_PUBLIC_KEY);
+      const options = { applicationServerKey, userVisibleOnly: true };
+      const subscription = await sw?.pushManager.subscribe(options);
+      await subscribe.mutateAsync({ subscription: JSON.stringify(subscription) });
+    } catch (err: any) {
+      showError(err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <Button
-      testId='push-notification-button' 
+      testId='push-notification-button'
       sx={{
         "&.Mui-disabled": {
-          backgroundColor: permission === 'granted' ? 
-            palette.success.main : 
-            permission === 'denied' ?
-               palette.error.main :
-               palette.grey[500],
+          backgroundColor: getButtonColor(),
           color: palette.primary.contrastText
         },
         paddingX: '12px',
@@ -62,3 +64,5 @@ export const PushNotificationButton : React.FC<PushNotificationButtonProps>= ({
     </Button>
   );
 };
+
+export default PushNotificationButton;
--- frontend/study-app/src/serviceworker/strategies/getData.ts
+++ frontend/study-app/src/serviceworker/strategies/getData.ts
@@ -1,39 +1,44 @@
 import { StrategyHandler } from 'workbox-strategies';
 import { addLastUpdatedParams } from '../utils/utils';
 import { AbstractStrategy } from './abstractStrategy';
+import { IDBPDatabase } from 'idb';
 
 export class GetData extends AbstractStrategy {
-
   constructor(dbName: string, indexName?: string) {
-    super(dbName,  indexName);
+    super(dbName, indexName);
   }
 
   protected async _handle(
-    request: Request, 
+    request: Request,
     handler: StrategyHandler
   ): Promise<Response | undefined> {
-    // create new last updated date to set at the end. Important to do it now 
-    // so you don't run the risk of it being newer than the actual data
     const newLastUpdated = new Date();
     const db = await this.dbPromise;
-    const lastUpdated = await this.getMetaData(db); 
-    const modifiedRequest = lastUpdated ? await addLastUpdatedParams(request, lastUpdated) : request.clone();
-    return handler.fetch(modifiedRequest)
-    .then(async (response) => {
+    const lastUpdated = await this.getMetaData(db);
+    const modifiedRequest = lastUpdated
+      ? await addLastUpdatedParams(request, lastUpdated)
+      : request.clone();
+
+    try {
+      const response = await handler.fetch(modifiedRequest);
+
       if (!response.ok) {
-        return await this.getFromDB(db);
+        return this.getFromDB(db);
       }
-      const responseClone = response.clone()
-      await responseClone.json().then(async (data) => {
-        const tx = db.transaction(this.storeName, 'readwrite');
-        await Promise.all(data.map((record: Record<string, any>) => {
-          return tx.store.put(record);
-        }));
-      });
+
+      const data = await response.clone().json();
+      await this.storeInDB(db, data);
+
       await this.setMetaData(db, newLastUpdated);
-      return await this.getFromDB(db);
-    }).catch(async () => {
-      return await this.getFromDB(db);
-    });
+      return this.getFromDB(db);
+    } catch (error) {
+      return this.getFromDB(db);
+    }
+  }
+
+  private async storeInDB(db: IDBPDatabase, data: Record<string, any>[]): Promise<void> {
+    const tx = db.transaction(this.storeName, 'readwrite');
+    const promises = data.map((record) => tx.store.put(record));
+    await Promise.all(promises);
   }
 }
