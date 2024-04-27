74b1d6bd7d4499a863235ad03209da6ba5652fc3
kaimannweiler <kai.mannweiler@hotmail.de>
2024-04-27 21:56:59 +0200

    show "geplant" when completed

--- frontend/study-app/src/modules/tasks/components/TaskItem/TaskItem.tsx
+++ frontend/study-app/src/modules/tasks/components/TaskItem/TaskItem.tsx
@@ -6,6 +6,8 @@ import dayjs from 'dayjs';
 import { getTaskState } from '@modules/tasks/utils';
 import { TaskDialog } from '..';
 import { useOpen } from '@modules/core/hooks';
+import { formatiOSDateShortWithTime } from '@modules/date/utils';
+import { useDateContext } from '@modules/date/contexts';
 
 export interface TaskItemProps {
   task: Task;
@@ -15,13 +17,18 @@ export const TaskItem : React.FC<TaskItemProps>= ({
   task,
 }) => {
   const { isOpen, open, close } = useOpen(false); 
+  const { date } = useDateContext();
   const state = useMemo(() => {
     return getTaskState(task);
   }, [task]);
 
-  const dateString = task.completedAt ? 
-    'abgeschlossen: ' + dayjs(task.completedAt).format('HH:mm') : 
-    'geplant: ' + dayjs(task.scheduledAt).format('HH:mm');
+  const completedDateString = task.completedAt ? ' - erledigt: ' + (
+    dayjs(task.completedAt).isSame(date, 'd') ? 
+    dayjs(task.completedAt).format('HH:mm') :
+    formatiOSDateShortWithTime(task?.completedAt) ):
+    '';
+
+  const dateString = 'geplant: ' + dayjs(task.scheduledAt).format('HH:mm') + completedDateString;
 
   return (
     <StyledListItem 
