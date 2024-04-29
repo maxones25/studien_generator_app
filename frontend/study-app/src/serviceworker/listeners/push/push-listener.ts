1954aa85a0f79d85e1d414eb49b24bf89992bf7d
kaimannweiler <kai.mannweiler@hotmail.de>
2024-04-29 13:04:21 +0200

    Fix Iphone bug where it removes permission for notifications

--- frontend/study-app/src/serviceworker/listeners/push/push-listener.ts
+++ frontend/study-app/src/serviceworker/listeners/push/push-listener.ts
@@ -1,15 +1,9 @@
-import { getDB } from "../../indexedDB/getDB";
-
 declare let self: ServiceWorkerGlobalScope;
 
 export const pushHandler = async (event: PushEvent) => {
   const text = event.data?.text() ?? '';
   const data = JSON.parse(text);
   const img = "/images/pwa-192x192.png";
-  const db = await getDB();
-  const subscription = await db.get('subscription', data.id);
-
-  if (Boolean(subscription))
   self.registration.showNotification(data.title, {
     body: data.message,
     icon: img,
